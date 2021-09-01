ea1a92c0d619be47ab23c9308ebf7e6ffff46291
Jarosloov <jarosloov@yandex.ru>
2021-09-01 20:18:41 +0500

    ADD

--- Platformer/Assets/Script/Enemy/EnamyPatrol.cs
+++ Platformer/Assets/Script/Enemy/EnamyPatrol.cs
@@ -1,13 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnamyPatrol : MonoBehaviour
 {
     [Header("Enemy Characteristics")]
     [SerializeField] private float speed;
-    [Space(10)]
-    
+    [SerializeField] private float sleepTime;
+    private float _time;
+    private bool _stop;
+    [Space(10)] [SerializeField] private float distance;
     [Header("Patrol points")]
-    [SerializeField] private Transform pointLeft, pointRight;
+    [SerializeField] private Transform pointLeft;
+    [SerializeField] private Transform pointRight;
+
+
+    
+    private void Start()
+    {
+        
+    }
+
+    private bool _moveRight;
+    private void Update()
+    {
+        var check = Physics2D.Raycast(transform.position, Vector2.left, distance);
+        
+        
+        
+        if (transform.position.x > pointRight.position.x)
+        {
+            _stop = true;
+            _moveRight = false;
+            StartCoroutine(StopPosition(false));
+                
+        }
+        else if (transform.position.x < pointLeft.position.x)
+        {
+            _stop = true;
+            _moveRight = true;
+            StartCoroutine(StopPosition(true));
+        }
+        print(_stop);
+
+        if (!_stop)
+        {
+            if (_moveRight)
+            {
+                print("+");
+                transform.position = new Vector2(transform.position.x + speed * Time.deltaTime , transform.position.y);
+                    
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x - speed * Time.deltaTime , transform.position.y);
+                print("-");
+            }
+        }
+        
+            
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+       // Gizmos.DrawRay(transform.position, attackRange);
+    }
+
+    private IEnumerator StopPosition(bool right)
+    {
+        
+       
+        yield return new WaitForSeconds(sleepTime);
+        print("+++");
+        _stop = false;
+    }
 }
